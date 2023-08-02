db48afe12b6ad5b5e2cf4859ad72e57a5dea581b
unknown <rapkshn@gmail.com>
2023-08-02 15:42:33 +0530

    duration added

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -51,22 +51,26 @@ export const SocialLinks = () => {
   return (
     <div className=" hidden  lg:flex flex-col top-[35%] left-0 fixed">
       <ul>
-
-        {links.map(({id, href, child, style, download})=>(
-        <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-0 bg-gray-500"+" "+style}>
-          <a
-            href={href}
-            className="flex justify-between items-center w-full text-white"
-            target="_blank"
-            rel="noreferrer"
-            download={download}
+        {links.map(({ id, href, child, style, download }) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+              " " +
+              style
+            }
           >
-                {child}
-          </a>
-        </li>
-
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              target="_blank"
+              rel="noreferrer"
+              download={download}
+            >
+              {child}
+            </a>
+          </li>
         ))}
-
       </ul>
     </div>
   );
