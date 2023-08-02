50c8f3d581b4ff6d6d6931f1a6ef82c75740210a
unknown <rapkshn@gmail.com>
2023-08-02 14:50:51 +0530

    Project DOne

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+        
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-5xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <p className='text-xl mt-16'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores porro officia non minus repellat ipsam velit nulla reprehenderit iure dolore deleniti quidem esse hic eum qui eaque facilis labore, consectetur expedita quaerat nobis nostrum alias! Quibusdam voluptate laborum doloremque? Iusto doloribus, voluptatem libero quis laudantium officia illum quo debitis blanditiis.
+            </p>
+            <br />
+            <p className='text-xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A veritatis voluptas, harum sed sint inventore officiis error, ipsa ut optio corrupti placeat quibusdam sunt voluptatum reiciendis veniam temporibus accusamus. Nisi iusto non sed nemo, ratione doloribus libero sunt explicabo sapiente expedita dolorem magni, fugiat atque, accusantium nesciunt consequuntur rerum quia.</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { HiOutlineMail } from "react-icons/hi";
+
+export const SocialLinks = () => {
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkedIn <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/rahul-prakasan-v-v-90843114a/",
+
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          Github <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/rapkshn47",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Mail <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mailto:rapkshn@gmail.com",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Resume <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: "/Resume.pdf",
+
+      style: "rounded-br-md",
+      download: true,
+    },
+  ];
+
+  return (
+    <div className=" hidden  lg:flex flex-col top-[35%] left-0 fixed">
+      <ul>
+
+        {links.map(({id, href, child, style, download})=>(
+        <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-0 bg-gray-500"+" "+style}>
+          <a
+            href={href}
+            className="flex justify-between items-center w-full text-white"
+            target="_blank"
+            rel="noreferrer"
+            download={download}
+          >
+                {child}
+          </a>
+        </li>
+
+        ))}
+
+      </ul>
+    </div>
+  );
+};
